5c3bbd8d0d1c8ad8fb206d44dd58a450ccaf6140
Emilio Moretti <emilio.moretti@jivesoftware.com>
2013-03-06 19:55:41 -0300

    Added scancodes

--- AutoHotPy.py
+++ AutoHotPy.py
@@ -36,11 +36,41 @@ class FunctionRunner(threading.Thread):
             self.queue.task_done()
             
 class Task(object):
-    def __init__(self,f,param):
+    def __init__(self,autohotpy,f,param):
         self.function = f
-        self.arg = param
+        self.arg1 = autohotpy
+        self.arg2 = param
     def run(self):
-        self.function(self.arg)
+        self.function(self.arg1,self.arg2)
+        
+class Key(object):
+    def __init__(self,auto,code):
+        self.auto = auto
+        self.code = code
+        
+    def up(self):
+        up = InterceptionKeyStroke()
+        up.code = self.code
+        up.state = InterceptionKeyState.INTERCEPTION_KEY_UP
+        self.auto.sendToDefaultKeyboard(up)
+        
+    def press(self):
+        event = InterceptionKeyStroke()
+        event.code = self.code
+        event.state = InterceptionKeyState.INTERCEPTION_KEY_DOWN
+        self.auto.sendToDefaultKeyboard(event)
+        time.sleep(self.auto.default_interval)
+        event.state = InterceptionKeyState.INTERCEPTION_KEY_UP
+        self.auto.sendToDefaultKeyboard(event)
+        
+    def down(self):
+        down = InterceptionKeyStroke()
+        down.code = self.code
+        down.state = InterceptionKeyState.INTERCEPTION_KEY_DOWN
+        self.auto.sendToDefaultKeyboard(down)
+        
+    def __int__(self):
+        return int(self.code)
         
 class AutoHotPy(object):
     
@@ -53,7 +83,6 @@ class AutoHotPy(object):
             raise Exception("Interception driver not installed!\nInstall required drivers to continue.")
             
         # Handlers
-        
         self.keyboard_handler_down = collections.defaultdict(self.__default_element)
         self.keyboard_handler_hold = collections.defaultdict(self.__default_element)
         self.keyboard_handler_up = collections.defaultdict(self.__default_element)
@@ -85,12 +114,198 @@ class AutoHotPy(object):
         
         self.exit_configured = False
         
+        #default interval between keypress
+        self.default_interval = 0.01
         #Threads queue
         self.kb_queue = Queue.Queue()
         self.mouse_queue = Queue.Queue()
         
+        # Default key scancodes (you can send your own anyway)
+        # WARNING! Most of these depend on the keyboard implementation!!!!
+        self.ESC=Key(self,0x1)
+        self.N1=Key(self,0x2)
+        self.N2=Key(self,0x3)
+        self.N3=Key(self,0x4)
+        self.N4=Key(self,0x5)
+        self.N5=Key(self,0x6)
+        self.N6=Key(self,0x7)
+        self.N7=Key(self,0x8)
+        self.N8=Key(self,0x9)
+        self.N9=Key(self,0x0A)
+        self.N0=Key(self,0x0B)
+        self.DASH=Key(self,0x0C)
+        #self.Err:520=Key(self,0x0D)
+        self.BACKSPACE=Key(self,0x0E)
+        self.TAB=Key(self,0x0F)
+        self.Q=Key(self,0x10)
+        self.W=Key(self,0x11)
+        self.E=Key(self,0x12)
+        self.R=Key(self,0x13)
+        self.T=Key(self,0x14)
+        self.Y=Key(self,0x15)
+        self.U=Key(self,0x16)
+        self.I=Key(self,0x17)
+        self.O=Key(self,0x18)
+        self.P=Key(self,0x19)
+        self.BRACKET_LEFT=Key(self,0x1A)
+        self.BRACKET_RIGHT=Key(self,0x1B)
+        self.ENTER=Key(self,0x1C)
+        self.CTRL=Key(self,0x1D)
+        self.A=Key(self,0x1E)
+        self.S=Key(self,0x1F)
+        self.D=Key(self,0x20)
+        self.F=Key(self,0x21)
+        self.G=Key(self,0x22)
+        self.H=Key(self,0x23)
+        self.J=Key(self,0x24)
+        self.K=Key(self,0x25)
+        self.L=Key(self,0x26)
+        self.SEMICOLON=Key(self,0x27)
+        self.APOSTROPHE=Key(self,0x28)
+        self.GRAVE_ACCENT=Key(self,0x29)
+        self.LEFT_SHIFT=Key(self,0x2A)
+        self.BACKSLASH=Key(self,0x2B)
+        self.Z=Key(self,0x2C)
+        self.X=Key(self,0x2D)
+        self.C=Key(self,0x2E)
+        self.V=Key(self,0x2F)
+        self.B=Key(self,0x30)
+        self.N=Key(self,0x31)
+        self.M=Key(self,0x32)
+        self.COMMA=Key(self,0x33)
+        self.DOT=Key(self,0x34)
+        self.SLASH=Key(self,0x35)
+        self.RIGHT_SHIFT=Key(self,0x36)
+        self.PRINT_SCREEN=Key(self,0x37)
+        self.ALT=Key(self,0x38)
+        self.SPACE=Key(self,0x39)
+        self.CAPSLOCK=Key(self,0x3A)
+        self.F1=Key(self,0x3B)
+        self.F2=Key(self,0x3C)
+        self.F3=Key(self,0x3D)
+        self.F4=Key(self,0x3E)
+        self.F5=Key(self,0x3F)
+        self.F6=Key(self,0x40)
+        self.F7=Key(self,0x41)
+        self.F8=Key(self,0x42)
+        self.F9=Key(self,0x43)
+        self.F10=Key(self,0x44)
+        self.NUMLOCK=Key(self,0x45)
+        self.SCROLLLOCK=Key(self,0x46)
+        self.HOME=Key(self,0x47)
+        self.UP_ARROW=Key(self,0x48)
+        self.PAGE_UP=Key(self,0x49)
+        self.DASH_NUM=Key(self,0x4A)
+        self.LEFT_ARROW=Key(self,0x4B)
+        self.NUMERIC_5 =Key(self,0x4C)
+        self.RIGHT_ARROW=Key(self,0x4D)
+        self.PLUS=Key(self,0x4E)
+        self.END=Key(self,0x4F)
+        self.DOWN_ARROW=Key(self,0x50)
+        self.PAGE_DOWN=Key(self,0x51)
+        self.INSERT=Key(self,0x52)
+        self.DELETE=Key(self,0x53)
+        self.SHIFT_F1=Key(self,0x54)
+        self.SHIFT_F2=Key(self,0x55)
+        self.SHIFT_F3=Key(self,0x56)
+        self.SHIFT_F4=Key(self,0x57)
+        self.SHIFT_F5=Key(self,0x58)
+        self.SHIFT_F6=Key(self,0x59)
+        self.SHIFT_F7=Key(self,0x5A)
+        self.SHIFT_F8=Key(self,0x5B)
+        self.SHIFT_F9=Key(self,0x5C)
+        self.SHIFT_F10=Key(self,0x5D)
+        self.CTRL_F1=Key(self,0x5E)
+        self.CTRL_F2=Key(self,0x5F)
+        self.CTRL_F3=Key(self,0x60)
+        self.CTRL_F4=Key(self,0x61)
+        self.CTRL_F5=Key(self,0x62)
+        self.CTRL_F6=Key(self,0x63)
+        self.CTRL_F7=Key(self,0x64)
+        self.CTRL_F8=Key(self,0x65)
+        self.CTRL_F9=Key(self,0x66)
+        self.CTRL_F10=Key(self,0x67)
+        self.ALT_F1=Key(self,0x68)
+        self.ALT_F2=Key(self,0x69)
+        self.ALT_F3=Key(self,0x6A)
+        self.ALT_F4=Key(self,0x6B)
+        self.ALT_F5=Key(self,0x6C)
+        self.ALT_F6=Key(self,0x6D)
+        self.ALT_F7=Key(self,0x6E)
+        self.ALT_F8=Key(self,0x6F)
+        self.ALT_F9=Key(self,0x70)
+        self.ALT_F10=Key(self,0x71)
+        self.CTRL_PRINT_SCREEN=Key(self,0x72)
+        self.CTRL_LEFT_ARROW=Key(self,0x73)
+        self.CTRL_RIGHT_ARROW=Key(self,0x74)
+        self.CTRL_END=Key(self,0x75)
+        self.CTRL_PAGE_DOWN=Key(self,0x76)
+        self.CTRL_HOME=Key(self,0x77)
+        self.ALT_1=Key(self,0x78)
+        self.ALT_2=Key(self,0x79)
+        self.ALT_3=Key(self,0x7A)
+        self.ALT_4=Key(self,0x7B)
+        self.ALT_5=Key(self,0x7C)
+        self.ALT_6=Key(self,0x7D)
+        self.ALT_7=Key(self,0x7E)
+        self.ALT_8=Key(self,0x7F)
+        self.ALT_9=Key(self,0x80)
+        self.ALT_0=Key(self,0x81)
+        self.ALT_DASH=Key(self,0x82)
+        self.ALT_EQUALS=Key(self,0x82)
+        self.CTRL_PAGE_UP=Key(self,0x84)
+        self.F11=Key(self,0x85)
+        self.F12=Key(self,0x86)
+        self.SHIFT_F11=Key(self,0x87)
+        self.SHIFT_F12=Key(self,0x88)
+        self.CTRL_F11=Key(self,0x89)
+        self.CTRL_F12=Key(self,0x8A)
+        self.ALT_F11=Key(self,0x8B)
+        self.ALT_F12=Key(self,0x8C)
+        self.CTRL_UP_ARROW=Key(self,0x8C)
+        self.CTRL_DASH_NUM=Key(self,0x8E)
+        self.CTRL_5_NUM=Key(self,0x8F)
+        self.CTRL_PLUS_NUM=Key(self,0x90)
+        self.CTRL_DOWN_ARROW=Key(self,0x91)
+        self.CTRL_INSERT=Key(self,0x92)
+        self.CTRL_DELETE=Key(self,0x93)
+        self.CTRL_TAB=Key(self,0x94)
+        self.CTRL_SLASH_NUM=Key(self,0x95)
+        self.CTRL_ASTERISK_NUM=Key(self,0x96)
+        self.ALT_HOME=Key(self,0x97)
+        self.ALT_UP_ARROW=Key(self,0x98)
+        self.ALT_PAGE_UP=Key(self,0x99)
+        #self. =Key(self,0x9A)
+        self.ALT_LEFT_ARROW=Key(self,0x9B)
+        #self. =Key(self,0x9C)
+        self.ALT_RIGHT_ARROW=Key(self,0x9D)
+        #self. =Key(self,0x9E)
+        self.ALT_END=Key(self,0x9F)
+        self.ALT_DOWN_ARROW=Key(self,0xA0)
+        self.ALT_PAGE_DOWN=Key(self,0xA1)
+        self.ALT_INSERT=Key(self,0xA2)
+        self.ALT_DELETE=Key(self,0xA3)
+        self.ALT_SLASH_NUM=Key(self,0xA4)
+        self.ALT_TAB=Key(self,0xA5)
+        self.ALT_ENTER_NUM=Key(self,0xA6)
+        
     def __default_element(self):
-            return None
+        """
+        Used to return None instead of a key exception for maps
+        """
+        return None
+            
+    def __null_handler(self,interception,event):
+        """
+        Used as a null handler to disable events like "hold"
+        """
+        return None
+        
+    def sleep(self):
+        """
+        Sleep default interval. useful for waiting between keypress.
+        """
+        time.sleep(self.default_interval)
             
     def start(self):
         if (not self.exit_configured):
@@ -125,7 +340,7 @@ class AutoHotPy(object):
                     
                         
                     if (user_function):
-                        self.kb_queue.put(Task(user_function,kb_event))
+                        self.kb_queue.put(Task(self,user_function,kb_event))
                     else:
                         self.interception.interception_send(self.context, device, ctypes.byref(stroke), 1)
                     
@@ -144,7 +359,7 @@ class AutoHotPy(object):
                         user_function = self.mouse_handler_hold[mouse_event.state]
                     
                     if (user_function):
-                        self.mouse_queue.put(Task(user_function,mouse_event))
+                        self.mouse_queue.put(Task(self,user_function,mouse_event))
                     else:
                         self.interception.interception_send(self.context, device, ctypes.byref(stroke), 1)
                 
@@ -197,7 +412,7 @@ class AutoHotPy(object):
     def isRunning(self):
         return self.running
         
-    def stop(self, event):
+    def stop(self, autohotpy, event):
         self.running = False
         
     def getKeyboardState(self, code):
@@ -208,16 +423,20 @@ class AutoHotPy(object):
     
     def registerExit(self, key):
         self.exit_configured = True
-        self.keyboard_handler_down[key] = self.stop
+        self.keyboard_handler_down[int(key)] = self.stop
         
     def registerForKeyDown(self, key, handler):
-        self.keyboard_handler_down[key] = handler
+        self.keyboard_handler_down[int(key)] = handler
+    
+    def registerForKeyDownAndDisableHoldEvent(self, key, handler):
+        self.keyboard_handler_down[int(key)] = handler
+        self.keyboard_handler_hold[int(key)] = self.__null_handler
     
     def registerForKeyUp(self, key, handler):
-        self.keyboard_handler_up[key] = handler
+        self.keyboard_handler_up[int(key)] = handler
         
     def registerForKeyHold(self, key, handler):
-        self.keyboard_handler_hold[key] = handler
+        self.keyboard_handler_hold[int(key)] = handler
         
     def sendToDefaultMouse(self, stroke):
         self.interception.interception_send(self.context, self.default_mouse_device, ctypes.byref(stroke), 1)
@@ -230,32 +449,16 @@ class AutoHotPy(object):
     
 if __name__=="__main__":
     auto = AutoHotPy()
-    SCANCODE_ESC = 0x01
-    auto.registerExit(SCANCODE_ESC)
-    def abc(event):
+    auto.registerExit(auto.ESC)
+    def abc(au,event):
         print("Abc running")
-        event.code = 0x1e
-        auto.sendToDefaultKeyboard(event)
-        event.state = 0x01    
-        auto.sendToDefaultKeyboard(event)
-        time.sleep(1)
-        b = InterceptionKeyStroke()
-        b.code = 0x30
-        b.state = 0x00
-        auto.sendToDefaultKeyboard(b)
-        b.state = 0x01        
-        auto.sendToDefaultKeyboard(b)
-        time.sleep(1)
-        c = InterceptionKeyStroke()
-        c.code = 0x2e
-        c.state = 0x00
-        auto.sendToDefaultKeyboard(c)
-        c.state = 0x01        
-        auto.sendToDefaultKeyboard(c)
+        au.A.press()
+        au.B.press()
+        au.C.press()
         
 
         
-    auto.registerForKeyDown(0x2e,abc)
+    auto.registerForKeyDownAndDisableHoldEvent(auto.C,abc)
     auto.start()
     
     
